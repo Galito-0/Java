9e3c945d9382c79bb13c6515d8d8dc3af21ba9b8
Galito-0 <galmohorko@gmail.com>
2025-10-24 00:16:32 +0200

    24.10.2025

--- 01_University/1_Vaje/Vaje3_23-10-25/Zgoscenke.java
+++ 01_University/1_Vaje/Vaje3_23-10-25/Zgoscenke.java
@@ -6,10 +6,26 @@ public class Zgoscenke {
         Scanner sc = new Scanner(System.in);
 
         int st_zgoscenk = sc.nextInt();
-        int velikost = sc.nextInt();
+        int prostor = sc.nextInt();
 
-        sc.nextLine();
+        int trenutna_zgoscenka = 1;
+        int trenutni_prostor = 0;
 
+        while (sc.hasNextInt() && trenutna_zgoscenka <= st_zgoscenk) {
+            int velikost_dat = sc.nextInt();
+
+            if (velikost_dat <= (prostor - trenutni_prostor)) {
+                trenutni_prostor += velikost_dat;
+                System.out.println(velikost_dat + " EP" + " -> " + "zgoscenka " + trenutna_zgoscenka + " (" + trenutni_prostor + " EP)");
+
+            } else {
+                trenutna_zgoscenka++;
+                if (trenutna_zgoscenka <= st_zgoscenk) {
+                    System.out.println(velikost_dat + " EP" + " -> " + "zgoscenka " + trenutna_zgoscenka + " (" + velikost_dat + " EP)");
+                }
+                trenutni_prostor = velikost_dat;
+            }
+        }
 
         sc.close();
     }
